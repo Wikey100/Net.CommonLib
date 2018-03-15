129c046558de46fdad8ebce8f655ab22ef2e1123
wikey <517537691@qq.com>
2018-03-15 15:18:19 +0800

    为各类库添加自定义日志处理类
    ActiveMqQueueLog.cs
    CacheManagerLog.cs
    ClockSyncLog.cs
    DBAccessLog.cs
    FtpLog.cs
    ICELog.cs
    MessageLog.cs
    PrintLog.cs
    PrismLog.cs
    SocketCommLog.cs
    UtilLog.cs
    WebsphereQueueLog.cs

---
+++ Net.CommonLib/Net.CommonLib.SocketComm/SocketCommLog.cs
@@ -0,0 +1,140 @@
+﻿/*******************************************************************
+ * * 文件名： SocketCommLog.cs
+ * * 文件作用： 日志记录
+ * *
+ * *-------------------------------------------------------------------
+ * *修改历史记录：
+ * *修改时间      修改人    修改内容概要
+ * *2018-03-15    xwj       新增
+ * *******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Net.CommonLib.Log;
+
+namespace Net.CommonLib.SocketComm
+{
+    public class SocketCommLog : ILog
+    {
+        public void Debug(string msg)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Debug(string msg, Exception e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Error(string msg)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Error(string msg, Exception e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Fatal(string msg)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Fatal(string msg, Exception e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Info(string msg)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Info(string msg, Exception e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Warn(string msg)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+
+        public void Warn(string msg, Exception e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message,ex);
+            }
+        }
+    }
+}
